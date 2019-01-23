c77346f3f1268b59b5936a7d11353b8b6b839986
Roland Zerek <rolandz@poczta.fm>
2019-01-23 07:43:44 +0100

    Fixed initialization to make it asynchronous (parallel)

--- Synchronization1/Program.cs
+++ Synchronization1/Program.cs
@@ -12,8 +12,8 @@ namespace Synchronization1
             var e = new ManualResetEvent(false);
             var waiter = new EventWaiter(e, TimeSpan.FromMilliseconds(1000));
             var targetService = new Someervice();
-            await InitializeSystem(e);
-            await InitializeSystem(e);
+            InitializeSystem(e, TimeSpan.FromMilliseconds(5000));
+            InitializeSystem(e, TimeSpan.FromMilliseconds(5000));
             var service = new ConditionalHolder(targetService, () => waiter.Wait());
             var tasks = Enumerable
                 .Range(1, 100)
@@ -22,10 +22,11 @@ namespace Synchronization1
             Console.WriteLine("Done all!");
         }
 
-        private static async Task InitializeSystem(EventWaitHandle @event)
+        private static async Task InitializeSystem(EventWaitHandle @event, TimeSpan duration)
         {
-            await Task.Delay(TimeSpan.FromMilliseconds(5000));
-            @event.Set();
+            await Task
+                .Delay(duration)
+                .ContinueWith(t => @event.Set());
         }
     }
     class EventWaiter
