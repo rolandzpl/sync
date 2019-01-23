bd88adaa80feda71dc5814d5c25312d2b820d5e5
Roland Zerek <rolandz@poczta.fm>
2019-01-23 07:29:10 +0100

    Initial commit

--- Synchronization1/Program.cs
+++ Synchronization1/Program.cs
@@ -1,12 +1,81 @@
 ﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Synchronization1
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var e = new ManualResetEvent(false);
+            var waiter = new EventWaiter(e, TimeSpan.FromMilliseconds(1000));
+            var targetService = new Someervice();
+            await InitializeSystem(e);
+            await InitializeSystem(e);
+            var service = new ConditionalHolder(targetService, () => waiter.Wait());
+            var tasks = Enumerable
+                .Range(1, 100)
+                .Select(i => service.DoSomeWork());
+            Task.WaitAll(tasks.ToArray());
+            Console.WriteLine("Done all!");
+        }
+
+        private static async Task InitializeSystem(EventWaitHandle @event)
+        {
+            await Task.Delay(TimeSpan.FromMilliseconds(5000));
+            @event.Set();
+        }
+    }
+    class EventWaiter
+    {
+        private readonly EventWaitHandle @event;
+        private readonly TimeSpan timeout;
+
+        public EventWaiter(EventWaitHandle @event, TimeSpan timeout)
+        {
+            this.@event = @event;
+            this.timeout = timeout;
+        }
+
+        public void Wait()
+        {
+            if (!@event.WaitOne(timeout))
+            {
+                throw new SystemUnititializedException();
+            }
+        }
+    }
+
+    class ConditionalHolder : IService
+    {
+        private readonly IService target;
+        private readonly Action wait;
+
+        public ConditionalHolder(IService target, Action wait)
+        {
+            this.target = target;
+            this.wait = wait;
+        }
+
+        public async Task DoSomeWork()
+        {
+            wait();
+            target.DoSomeWork();
+        }
+    }
+
+    interface IService
+    {
+        Task DoSomeWork();
+    }
+
+    class Someervice : IService
+    {
+        public async Task DoSomeWork()
+        {
+            Console.WriteLine("Did some work here...");
         }
     }
 }
---
+++ Synchronization1/SystemUnititializedException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Synchronization1
+{
+    [Serializable]
+    internal class SystemUnititializedException : Exception
+    {
+        public SystemUnititializedException()
+        {
+        }
+
+        public SystemUnititializedException(string message) : base(message)
+        {
+        }
+
+        public SystemUnititializedException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected SystemUnititializedException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
